d7d6b7b66b6fd3dc4e08921824b043e2eb379929
1213ai <1192976c@gmail.com>
2021-10-11 14:06:03 +0000

    add user

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '233a6c3557def1786c9bebc2e924018408835e49ba9b8e68b00cf710471ff7f19d3619666fb8339d4fdfb50ccc2f4caf131d99e903fc3242c8316db59250fd71'
