254a45d1ec7f662268b70df36c88019a1302e408
1213ai <1192976c@gmail.com>
2021-10-13 03:49:16 +0000

    start/star

---
+++ db/migrate/20211012085026_add_columns_to_recipes.rb
@@ -0,0 +1,9 @@
+class AddColumnsToRecipes < ActiveRecord::Migration[5.2]
+  def change
+    add_column :recipes, :sweetness, :float
+    add_column :recipes, :spicy, :float
+    add_column :recipes, :salty, :float
+    add_column :recipes, :bitter_taste, :float
+    add_column :recipes, :acidity, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_142945) do
+ActiveRecord::Schema.define(version: 2021_10_12_085026) do
 
   create_table "how_to_makes", force: :cascade do |t|
     t.integer "recipe_id"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 2021_10_11_142945) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "sweetness"
+    t.float "spicy"
+    t.float "salty"
+    t.float "bitter_taste"
+    t.float "acidity"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
